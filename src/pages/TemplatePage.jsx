8a596132c1a7b91b974b7afb2f99ecc64626c2f2
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2024-09-18 12:13:46 +0500

    "Added title object to HeroSectionWithImg component in TemplatePage"

--- src/pages/TemplatePage.jsx
+++ src/pages/TemplatePage.jsx
@@ -3,9 +3,14 @@ import HeroSectionWithImg from "../components/HeroSectionWithImg";
 import TempleteContentSection from "../components/TempleteContentSection";
 
 function TemplatePage({ language }) {
+  const title = {
+    en: "Template Page",
+    ar: "صفحة القالب",
+  };
+
   return (
     <div>
-      <HeroSectionWithImg Title={{}} language={language} />
+      <HeroSectionWithImg Title={title} language={language} />
       <TempleteContentSection language={language} />
     </div>
   );
