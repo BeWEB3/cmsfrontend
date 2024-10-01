c56b79e1f70b0f6656d19aedc337898825f631be
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2024-10-01 16:16:54 +0500

    "Lazy load pages and add Suspense fallback"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage.jsx";
-import NewsPage from "./pages/NewsPage.jsx";
-import SubNewsPage from "./pages/SubNewsPage.jsx";
-import ContactUsPage from "./pages/ContactUsPage.jsx";
-import MembersPage from "./pages/MembersPage.jsx";
-// import TemplatePage from "./pages/TemplatePage.jsx";
 import ScrollToTop from "./hooks/ScrollToTop.jsx";
 import NotFound from "./pages/NotFoundPage.jsx";
-import DynamicPage from "./pages/DynamicPage.jsx";
+
+// Lazy load the pages
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const NewsPage = lazy(() => import("./pages/NewsPage.jsx"));
+const SubNewsPage = lazy(() => import("./pages/SubNewsPage.jsx"));
+const ContactUsPage = lazy(() => import("./pages/ContactUsPage.jsx"));
+const MembersPage = lazy(() => import("./pages/MembersPage.jsx"));
+// const TemplatePage = lazy(() => import("./pages/TemplatePage.jsx"));
+const DynamicPage = lazy(() => import("./pages/DynamicPage.jsx"));
 
 const App = () => {
   const [language, setLanguage] = useState(() => {
@@ -37,31 +39,33 @@ const App = () => {
         <Header language={language} toggleLanguage={toggleLanguage} />
 
         <main className="flex-grow w-full   ">
-          <Routes>
-            <Route path="/" element={<HomePage language={language} />} />
-            <Route path="/news" element={<NewsPage language={language} />} />
-            <Route
-              path="/news/:uid"
-              element={<SubNewsPage language={language} />}
-            />
-            <Route
-              path="/contactus"
-              element={<ContactUsPage language={language} />}
-            />
-            <Route
-              path="/network-presidency"
-              element={<MembersPage language={language} />}
-            />
-            {/* <Route
+          <Suspense fallback={<></>}>
+            <Routes>
+              <Route path="/" element={<HomePage language={language} />} />
+              <Route path="/news" element={<NewsPage language={language} />} />
+              <Route
+                path="/news/:uid"
+                element={<SubNewsPage language={language} />}
+              />
+              <Route
+                path="/contactus"
+                element={<ContactUsPage language={language} />}
+              />
+              <Route
+                path="/network-presidency"
+                element={<MembersPage language={language} />}
+              />
+              {/* <Route
               path="/template"
               element={<TemplatePage language={language} />}
             /> */}
-            <Route
-              path="page/:slug"
-              element={<DynamicPage language={language} />}
-            />
-            <Route path="*" element={<NotFound language={language} />} />
-          </Routes>
+              <Route
+                path="page/:slug"
+                element={<DynamicPage language={language} />}
+              />
+              <Route path="*" element={<NotFound language={language} />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer language={language} />
       </div>
